130c4bdd31a3e56d3f30c3ab769709625041cc74
TheMuslimPerson <lawenqader2010@gmail.com>
2024-04-25 22:00:04 +0200

    Remaking

--- Main/JS/Types/Index.d.ts
+++
@@ -1,9 +0,0 @@
-declare class Button_Handler {
-    Element: any;
-    constructor(Element: any);
-    Button_Redirct(URL: string): boolean;
-}
-declare const _default: {
-    Button_Handler: typeof Button_Handler;
-};
-export default _default;
--- Main/JS/Types/Redirct.d.ts
+++
@@ -1,5 +0,0 @@
-declare function Redirct(GET_URL: string): boolean;
-declare const _default: {
-    Redirct: typeof Redirct;
-};
-export default _default;
